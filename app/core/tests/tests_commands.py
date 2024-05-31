827cb7e6ba79deef20ad490bff428654167830c4
jhinckley <me@johnhinckley.net>
2024-05-31 16:32:40 -0700

    fixed blank lines

--- app/core/tests/tests_commands.py
+++ app/core/tests/tests_commands.py
@@ -13,17 +13,20 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """test commands"""
     def test_wait_for_db_ready(self, patched_check):
-        # test waiting for db.
+        """test waiting for db."""
         patched_check.return_value = True
-        call_command('wait_for_db')
-        patched_check.assert_called_once_with(databases=['default'])
 
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        # test waiting for db when getting operational error.
+        """test waiting for db when getting operational error."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
+
         call_command('wait_for_db')
+
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
